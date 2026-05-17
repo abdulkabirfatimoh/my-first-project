18c899577b991229ab7d6498e79603befd9adee4
abdulkabirfatimoh <fatimohabdulkabir40@gmail.com>
2026-05-16 22:46:09 -0700

    Add files via upload

---
+++ fatimah.js
@@ -0,0 +1,10 @@
+let age = 19;
+const name = "Abdulkabir fatimoh"
+const favouriteprogramminglanguage = "python"
+
+
+console.log(
+    "my name " + name + " i am " + age +
+    " years old, my favourite programming language is " + 
+    favouriteprogramminglanguage
+)
